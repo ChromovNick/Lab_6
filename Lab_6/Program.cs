33666cb8f685d9755ea7faeb7eea676e41d362b2
Никита Хромов <chromovnick@gmail.com>
2021-11-28 21:56:59 +0300

    Добавьте файлы проекта.

---
+++ Lab_6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку для определения самого длинного слова");
+            string sentence = Console.ReadLine();
+            sentence = sentence.ToLower();
+            string[] stringArray = sentence.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int max = 0;
+            int wordnumber = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > max)
+                {
+                    wordnumber = i;
+                    max = stringArray[i].Length;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в строке: {0}", stringArray[wordnumber]);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab_6_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ведите слово или строку для проверки на палиндром");
+            string sentence = Console.ReadLine();
+            sentence = sentence.ToLower();
+            string [] stringArray = sentence.Split(new Char[] { ' ', ',', '.', ':', '!', '?', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            string joined = string.Join("", stringArray);
+            string empty = String.Empty;
+            for (int i = joined.Length - 1; i >= 0; i--)
+            {
+                empty += joined[i];
+            }
+            Console.WriteLine(joined.Equals(empty) ? "Вы ввели палиндром!" : "Вы ввели не палиндром!");
+            Console.ReadKey();
+        }
+    }
+}
